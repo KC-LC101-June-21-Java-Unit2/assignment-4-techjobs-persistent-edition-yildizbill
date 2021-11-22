495914f6154e8335c689a7bcd424836bb6479d87
yildizbill <yildizbill99@gmail.com>
2021-11-22 00:33:51 -0600

    First Check

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,29 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.util.*;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @NotNull
+    @Size(max=500)
+    public String description;
+
+    @ManyToMany(mappedBy = "skills")
+    private List<Job> jobs = new ArrayList<>();
+
+    //Setters and Getters
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
+
+    //No arg Constructor
+    public Skill(){}
 }
